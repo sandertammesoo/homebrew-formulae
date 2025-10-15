ec653e72ca1479d0e56f89e0a8ae47a987f9e367
Sander Tammesoo <sander.tammesoo@gmail.com>
2025-10-16 02:40:09 +0300

    Fix SHA256 hash for zunit v0.11.0
    Update SHA256 hash to the correct value calculated from the v0.11.0 tarball

--- zunit.rb
+++ zunit.rb
@@ -2,8 +2,8 @@ class Zunit < Formula
   desc "A fork/adaption of a powerful testing framework for ZSH projects"
   homepage "https://zunit.xyz"
   url "https://github.com/sandertammesoo/zunit.git",
-      tag:      "v0.10.0"
-  sha256 "9d2cfe586d160451cfff87293a39095c61684bfdb813db689edd2d0c83fbaaa4"
+      tag:      "v0.11.0"
+  sha256 "6b8daf52143b0d4666d7e8c97f93f0fd511d345f966e7a47a6779ed01cedf112"
   head "https://github.com/sandertammesoo/zunit.git", branch: "develop" # the default is "master"
   license "MIT"
 
